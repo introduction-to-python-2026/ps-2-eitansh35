4c8759b15cccba34a285c9a87ee836d117f61f25
eitansh35 <eitansh35@gmail.com>
2025-11-06 11:52:18 +0200

    Refactor find_max_number and find_mean functions

--- math_utils.py
+++ math_utils.py
@@ -1,16 +1,17 @@
 def find_max_number(num1, num2, num3):
-     if num1>num2 and num1>num3:
-      print(num1) 
-    elif num2>num1 and num2>num3:
-      print(num2)
-    elif num3>num1 and num3>num2:
-      print(num3)  # Replace 'pass' with code
+    if num1>=num2 and num1>=num3:
+      return(num1) 
+    elif num2>=num1 and num2>=num3:
+      return(num2)
+    elif num3>=num1 and num3>=num2:
+      return(num3)  # Replace 'pass' with code
   # Replace 'pass' with code
 
 def find_mean(num1, num2, num3):
-    print((num1+num2+num3)/3)  # Replace 'pass' with code
+    return ((num1+num2+num3)/3)  # Replace 'pass' with code
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    pass  # Replace 'pass' with code
+    sigma = (((num1-mean)**2 +(num2-mean)**2 +(num3-mean)**2)/3)**0.5
+    return mean, sigma # Replace 'pass' with code
 
